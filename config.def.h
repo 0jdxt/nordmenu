7f0dedf6604460f1bb58451fc8ae41e5d117a143
Jay <jytrn@protonmail.com>
2020-10-20 03:39:44 +0100

    apply patches

--- config.def.h
+++ config.def.h
@@ -2,9 +2,11 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
     "Fira Sans Retina:size=12",
+    "Symbola:size=15",
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -17,8 +19,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 0;
+static unsigned int lines      = 10;
+static unsigned int lineheight = 30;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -27,4 +29,4 @@ static unsigned int lineheight = 0;
 static const char worddelimiters[] = " ";
 
 /* window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
