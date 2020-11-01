0b346b9fd78bd0da4986f0bab3397e7caf2afe97
Jay <jytrn@protonmail.com>
2020-11-01 13:55:52 +0000

    cpplint and vertfull patch

--- config.def.h
+++ config.def.h
@@ -1,25 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-    "Fira Sans Retina:size=12",
-    "Symbola:size=15",
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"Fira Sans Retina:size=12", "Symbola:size=15",
+                              "monospace:size=10"};
+static const char *prompt; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel] = { "#eceff4", "#434c5e" },
-    [SchemeSelHighlight] = { "#8fbcbb", "#434c5e" },
-    [SchemeNormHighlight] = { "#88c0d0", "#2e3440" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#d8dee9", "#2e3440"},
+    [SchemeSel] = {"#eceff4", "#434c5e"},
+    [SchemeSelHighlight] = {"#8fbcbb", "#434c5e"},
+    [SchemeNormHighlight] = {"#88c0d0", "#2e3440"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+static unsigned int lines = 10;
 static unsigned int lineheight = 30;
 
 /*
