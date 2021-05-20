c459c1973881488f28934b5c37e9e5e944846e4d
Jay <jytrn@protonmail.com>
2021-05-20 04:06:13 +0100

    font update

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"VictorMono Nerd Font:style=Medium:size=11",
+static const char *fonts[] = {"Victor Mono Nerd Font:style=Medium:size=10",
                               "Mutant Standard Emoji:size=15",
                               "Noto Color Emoji:size=11"};
 static const char *prompt; /* -p  option; prompt to the left of input field */
